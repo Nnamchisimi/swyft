4a2235ed1cfddee4c5b8d09134c2e14c01e0bcec
nnamchisimi <mosimiloluwannamchi@gmail.com>
2025-09-12 10:54:30 +0300

    socket updt

--- src/socket.js
+++ src/socket.js
@@ -1,9 +1,9 @@
-// socket.js
 import { io } from "socket.io-client";
 
-// Use environment variable for deployment
-const socket = io(process.env.REACT_APP_SOCKET_URL || "http://localhost:5000", {
-  transports: ["websocket"], // ensures real-time without fallbacks
+const SOCKET_URL = process.env.REACT_APP_SOCKET_URL;
+
+const socket = io(SOCKET_URL, {
+  transports: ["websocket"],
 });
 
 export default socket;
